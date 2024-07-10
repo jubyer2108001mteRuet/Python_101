dadb6591a1c3a9fe8effdbe06d788cf7477a3d9c
Jubyer <jubyer.2108001.ruet.mte@gmail.com>
2024-07-11 01:54:35 +0600

    via pycharm

---
+++ list.py
@@ -0,0 +1,21 @@
+
+#names=["John","Cypher","Kayo"]
+# print(names[0])
+#print(names)
+#list methods
+
+numbers= [1,2,3,4,5]
+numbers.append(6)
+numbers.insert(0,69)
+numbers.remove(1)
+#print(numbers)
+#numbers.clear()
+#print(numbers)
+
+#for item in numbers:
+    #print(item)
+#for i in range(5,10):
+ #   print(i)
+numbers= range(1,10,3)
+for number in numbers:
+    print(number)
---
+++ tuples.py
@@ -0,0 +1,3 @@
+numbers=(1,2,3,3)
+print(numbers)
+print(numbers.index(3))
